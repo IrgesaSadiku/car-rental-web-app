01797cfcec7f995aa198499d10f1f2fb3964a248
IrgesaSadiku <irgesasadiku@gmail.com>
2024-02-11 11:48:30 +0100

    Database Configuration and creating entities

---
+++ src/main/java/com/sda/carrental/models/BranchEntity.java
@@ -0,0 +1,30 @@
+package com.sda.carrental.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@Table(name = "branch")
+@NoArgsConstructor
+@AllArgsConstructor
+@jakarta.persistence.Entity
+
+
+public class BranchEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @jakarta.persistence.Column(name = "branch_id")
+    private Integer branch_id;
+    @Column(name= "address")
+    private String address;
+
+    @Column(name= "city")
+    private String city;
+
+    @ManyToOne( fetch = FetchType.EAGER)
+    @JoinColumn(name = "rental_id", nullable = false)
+    private RentalEntity rentalEntity;
+
+}
